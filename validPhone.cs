49f22cccb054a8c748d660cec7df944020d49669
wedginet <wedginet@gmail.com>
2024-09-04 19:34:27 -0500

    Update validPhone.cs

--- validPhone.cs
+++ validPhone.cs
@@ -1,3 +1,13 @@
+/*
+    Example Valid Phone Numbers:
++1 (555) 123-4567
+(555) 123-4567
+555-1234
+123-456-7890
++44 20 1234 5678 ext 123
++91 (80) 12345678 x123
+123-4567 ext 12
+*/
 using System.Text.RegularExpressions;
 
 public bool IsValidPhoneNumber(string candidate)

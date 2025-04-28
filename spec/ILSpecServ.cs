effbfc06789073b15893d9783b2252d71b728ef6
wedginet <wedginet@gmail.com>
2025-04-28 13:42:41 -0500

    Create ILSpecServ.cs

---
+++ spec/ILSpecServ.cs
@@ -0,0 +1,10 @@
+public interface ILookupSpeciesService
+{
+    Task<List<SpeciesDto>>  GetAllAsync();
+    Task<List<SpeciesDto>>  SearchByNameAsync(string name);
+    Task<List<SpeciesDto>>  GetChangedSinceAsync(DateTime since);
+    Task<SpeciesDto>        CreateAsync(SpeciesDto dto);
+    Task<bool>              UpdateAsync(SpeciesDto dto);
+    Task<bool>              DeleteAsync(int id);
+    Task<BulkInsertResult>  BulkInsertAsync(IEnumerable<SpeciesDto> dtos);
+}

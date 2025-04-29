926f1f194bc8d3b2764ed199f9ba9f62a64cbd72
wedginet <wedginet@gmail.com>
2025-04-29 08:17:48 -0500

    Update LSpecServ.cs

--- spec/LSpecServ.cs
+++ spec/LSpecServ.cs
@@ -1,41 +1,47 @@
-public class LookupSpeciesService : ILookupSpeciesService
+using LookupAdminApp.Models;
+using System.Net.Http.Json;
+
+namespace LookupAdminApp.Services
 {
-    private readonly HttpClient _http;
-
-    public LookupSpeciesService(HttpClient http)
-        => _http = http;
-
-    public async Task<List<SpeciesDto>> GetAllAsync()
-        => await _http.GetFromJsonAsync<List<SpeciesDto>>("api/lookupSpecies");
-
-    public async Task<List<SpeciesDto>> SearchByNameAsync(string name)
-        => await _http.GetFromJsonAsync<List<SpeciesDto>>($"api/lookupSpecies?name={Uri.EscapeDataString(name)}");
-
-    public async Task<List<SpeciesDto>> GetChangedSinceAsync(DateTime since)
-        => await _http.GetFromJsonAsync<List<SpeciesDto>>($"api/lookupSpecies/changed?lastUpdatedOn={since:o}");
-
-    public async Task<SpeciesDto> CreateAsync(SpeciesDto dto)
+    public class LookupSpeciesService : ILookupSpeciesService
     {
-        var resp = await _http.PostAsJsonAsync("api/lookupSpecies", dto);
-        resp.EnsureSuccessStatusCode();
-        return await resp.Content.ReadFromJsonAsync<SpeciesDto>()!;
-    }
+        private readonly HttpClient _http;
 
-    public async Task<bool> UpdateAsync(SpeciesDto dto)
-    {
-        var resp = await _http.PutAsJsonAsync($"api/lookupSpecies/{dto.Id}", dto);
-        return resp.IsSuccessStatusCode;
-    }
+        public LookupSpeciesService(HttpClient http) => _http = http;
 
-    public async Task<bool> DeleteAsync(int id)
-    {
-        var resp = await _http.DeleteAsync($"api/lookupSpecies/{id}");
-        return resp.IsSuccessStatusCode;
-    }
+        public Task<List<SpeciesDto>> GetAllAsync()
+            => _http.GetFromJsonAsync<List<SpeciesDto>>("api/lookupSpecies")!;
 
-    public async Task<BulkInsertResult> BulkInsertAsync(IEnumerable<SpeciesDto> dtos)
-    {
-        var resp = await _http.PostAsJsonAsync("api/lookupSpecies/bulk", dtos);
-        return await resp.Content.ReadFromJsonAsync<BulkInsertResult>()!;
+        public Task<List<SpeciesDto>> SearchByNameAsync(string name)
+            => _http.GetFromJsonAsync<List<SpeciesDto>>($"api/lookupSpecies?name={Uri.EscapeDataString(name)}")!;
+
+        public Task<List<SpeciesDto>> GetChangedSinceAsync(DateTime since)
+            => _http.GetFromJsonAsync<List<SpeciesDto>>($"api/lookupSpecies/changed?lastUpdatedOn={since:o}")!;
+
+        public async Task<SpeciesDto> CreateAsync(SpeciesDto dto)
+        {
+            var resp = await _http.PostAsJsonAsync("api/lookupSpecies", dto);
+            resp.EnsureSuccessStatusCode();
+            return await resp.Content.ReadFromJsonAsync<SpeciesDto>()!;
+        }
+
+        public async Task<bool> UpdateAsync(SpeciesDto dto)
+        {
+            var resp = await _http.PutAsJsonAsync($"api/lookupSpecies/{dto.Id}", dto);
+            return resp.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var resp = await _http.DeleteAsync($"api/lookupSpecies/{id}");
+            return resp.IsSuccessStatusCode;
+        }
+
+        public async Task<BulkInsertResult> BulkInsertAsync(IEnumerable<SpeciesDto> dtos)
+        {
+            var resp = await _http.PostAsJsonAsync("api/lookupSpecies/bulk", dtos);
+            resp.EnsureSuccessStatusCode();
+            return await resp.Content.ReadFromJsonAsync<BulkInsertResult>()!;
+        }
     }
 }

7d4a05ba0756c6a3a7ea0b9bf6dcbdab3e39673c
wedginet <wedginet@gmail.com>
2025-05-11 17:56:24 -0500

    Update ProgMod.cs

--- spec/ProgMod.cs
+++ spec/ProgMod.cs
@@ -1,3 +1,29 @@
-builder.Services.AddScoped<ILookupSpeciesService, LookupSpeciesService>();
+using LookupAdmin.Server.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+// point HttpClient at your API:
 builder.Services.AddHttpClient<ILookupSpeciesService, LookupSpeciesService>(client =>
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]));
+{
+    client.BaseAddress = new Uri("http://localhost:5110/");
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
